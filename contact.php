9d5cf7b0b0a3cc54a87b56c37ce3de1d007693ad
supphakon2545 <supphakon.r@rmutsvmail.com>
2025-01-08 09:30:35 +0700

    แก้ไขไฟล์

--- contact.php
+++ contact.php
@@ -17,6 +17,8 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
+                <li><a href="contact.php">เพิ่่มรายละเอียด</li>
+
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
